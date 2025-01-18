fb2455656e8c8ea7889df2adb4b6002eee8a6a5c
mariacamper <mariathetech180@gmail.com>
2025-01-17 22:14:24 -0500

    Adding JavaScript

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+function changeTheme (){
+    let body = document.querySelector("body");
+        if (body.classList.contains ("dark")) {
+           body.classList.remove("dark");
+        } else {
+            body.classList.add("dark");
+        }
+}
+let themeButton = document.querySelector(".changeThemeBtn");
+themeButton.addEventListener("click", changeTheme);
 
+if (yes){
+    alert("Thanks! I will work on it") {
+        else{
+            alert("I appreciate your honest response")
+        }
+    }
+}
